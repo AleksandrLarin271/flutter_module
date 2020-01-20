c1386f307b63e334596e8f2277ae82791a393259
Kollkaa <1kolia1@ukr.net>
2020-01-20 15:47:21 +0200

    kolia

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
@@ -112,22 +114,101 @@ class PageFour extends MaterialPageRoute<Null>{
           )
         ],
       ),
-      body:new Container(
-          child: new Center(
-            child: new Column(
-              mainAxisAlignment:MainAxisAlignment.center,
+      body: new Column(
+
               children:<Widget>[
-                new TextField(
+
+                 new Row(
+                    children: <Widget>[
+                      new Container(
+                        padding: const EdgeInsets.only(top: 40,left:20),
+                        child:new Text("Add Job"),
+                      ),
+                  ]),
+                new Row(
+                    children: <Widget>[
+                      Container(
+                          padding: const EdgeInsets.only(top:20 ,left:20),
+                          child:new Text("First, add a business to indicate "+
+                              "your boiler type.")),
+                    ]),
+
+                new Container(
+
+                  padding: const EdgeInsets.only(left: 20,right: 20),
+                  child:new TextField(
                   decoration: new InputDecoration(
                     hintText: "Hey Larin Chyhka"
                   ),
 
-                  ),
-                  new Text("")
+                  )),
+                  new Row(
+                    children: <Widget>[
+                      Container(
+                       child:new Text("Select Earnings Type"),
+                        padding: const EdgeInsets.only(left: 20, top : 10),
 
+                     ),
+
+                   ]
+                   ),
+                new Row(
+                    children: <Widget>[
+                      Container(
+                        width: 200,
+                        child:new Text("per diem"),
+                        padding: const EdgeInsets.only(left: 20, top : 10),
+                      ),
+                      Container (
+                        padding: const EdgeInsets.only(right: 20),
+                        width: 200.0,
+                          child: TextField(
+                        decoration: const InputDecoration(hintText: "50 \$"),
+
+                      )),
+
+                    ]
+                ),
+                new Row(
+                    children: <Widget>[
+                      Container(
+                        width: 200,
+                        child:new Text("hourly wages"),
+                        padding: const EdgeInsets.only(left: 20, top : 10),
+                      ),
+                      Container (
+                          padding: const EdgeInsets.only(right: 20),
+                          width: 200.0,
+                          child: TextField(
+                            decoration: const InputDecoration(hintText: "5 \$"),
+
+                          )),
+
+                    ]
+                ),
+                new Row(
+                    children: <Widget>[
+                      Container(
+                        width: 200,
+                        child:new Text("manual entry"),
+                        padding: const EdgeInsets.only(left: 20, top : 10),
+                      ),
+                      Container (
+                          padding: const EdgeInsets.only(right: 20),
+                          width: 200.0,
+                          child: TextField(
+                            decoration: const InputDecoration(hintText: "0"),
+
+                          )),
+
+                    ]
+                ),
+                new Container(
+                    child: FlatButton(
+                      child: Text('Saved',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0)),
+                    ),
+                ),
               ]
-            ),
-          ),
       )
 
 
